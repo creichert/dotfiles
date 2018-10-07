99c830193c970b3a310b60fa0a6ea0bdcf0e1e90
Christopher Reichert <creichert07@gmail.com>
2018-10-07 01:17:35 -0500

    Update xmonad config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -38,13 +38,14 @@ main = do
                                manageDocks
                              , namedScratchpadManageHook scratchpads
                              , className =? "Chromium" --> doF (W.shift "8")
-                             -- , title =? "org-capture" --> doFloat
+                             , className =? "Spotify"  --> doF (W.shift "9")
                              , manageHook def
                              ]
              , logHook = dynamicLogWithPP $ xpp h
              , startupHook = runStartupHook
              , handleEventHook = mconcat [ docksEventHook
                                          , handleEventHook def ]
+             , modMask = mod4Mask
              }
   where
     xpp h = xmobarPP {
@@ -67,38 +68,38 @@ newKeys xPCfg x = keys' x `Map.union` keys def x
     xpCfg = xPCfg { font = "xft:monofur:size=10:style=italic" }
     keys' XConfig { XMonad.modMask = modm } = Map.fromList [
 
-          ((modm,               xK_F1), spawn "xterm")
-        , ((modm .|. shiftMask, xK_F1), spawn "emacs -fs")
-        , ((modm .|. shiftMask, xK_F2), spawn "emacsclient -c")
-
+          ((modm, xK_F1), spawn "xterm")
         , ((0, xF86XK_Launch1), spawn "xterm")
 
-          -- launch dmenu
+        -- dmenu w/ history (see bin/dmenu_run_history)
+        -- - C-n : next in history
+        -- - C-p : prev in history
         , ((modm, xK_p), spawn "dmenu_run_history -nb '#3b3228' -nf '#d0c8c6' -fn 'monofur-11'")
 
-
-        , ((modm, xK_f), toggleFloatAllNew)
-
-          -- Audio
-        , ((0, 0x1008ff12), spawn "amixer set Master toggle")
-
-          -- Pass
-        , ((modm .|. shiftMask, xK_w), passPrompt xpCfg )
-        , ((modm .|. shiftMask, xK_s),  sshPrompt xpCfg)
-
-          -- Scratchpads (https://pbrisbin.com/tags/xmonad/)
-        , ((modm .|. shiftMask, xK_k),  namedScratchpadAction scratchpads "terminal")
-        , ((modm .|. shiftMask, xK_j),  namedScratchpadAction scratchpads "terminal2")
+        -- audio (amixer & mpris)
         , ((0, 0x1008ff11), spawn "amixer -c 0 set 'Master,0' 5%- > /dev/null")
         , ((0, 0x1008ff13), spawn "amixer -c 0 set Master 5%+ unmute > /dev/null")
-        , ((modm .|. shiftMask, xK_e),  namedScratchpadAction scratchpads "emacs")
+        , ((0, 0x1008ff12), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
+        , ((modm, 0x1008ff11), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
+        , ((modm, 0x1008ff13), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
+
+        -- quick emacs access
+        , ((modm .|. shiftMask, xK_F1), spawn "emacs -fs")
+        , ((modm .|. shiftMask, xK_F2), spawn "emacsclient -c")
+        -- pass
+        , ((modm .|. shiftMask, xK_w), passPrompt xpCfg )
+        , ((modm .|. shiftMask, xK_s), sshPrompt xpCfg)
+        -- scratchpads
+        , ((modm .|. shiftMask, xK_k),  namedScratchpadAction scratchpads "terminal")
+        , ((modm .|. shiftMask, xK_j),  namedScratchpadAction scratchpads "terminal2")
         , ((modm .|. shiftMask, xK_g),  namedScratchpadAction scratchpads "ghci")
         , ((modm .|. shiftMask, xK_r),  namedScratchpadAction scratchpads "psql")
-        , ((modm .|. shiftMask, xK_m),  namedScratchpadAction scratchpads "gnus")
+        , ((modm, xK_k),  namedScratchpadAction scratchpads "emacs")
+        , ((modm, xK_o),  namedScratchpadAction scratchpads "spotify")
+        , ((modm, xK_m),  namedScratchpadAction scratchpads "gnus")
 
           -- Take a selective screenshot using the command specified by mySelectScreenshot.
         , ((modm .|. shiftMask, xK_p), spawn screenshotRegion)
-
         ]
 
 screenshotRegion :: String
@@ -117,8 +118,8 @@ scratchpads = [
     , NS "terminal2" "xterm -T terminal2" (title =? "terminal2")
           (customFloating $ W.RationalRect (1/10) (1/5) (3/4) (6/10))
 
-    , NS "emacs" "emacs --title floater" (title =? "floater")
-          (customFloating $ W.RationalRect (1/10) (1/10) (8/10) (8/10))
+    , NS "emacs" "emacs --title org" (title =? "org")
+          (customFloating $ W.RationalRect (1/20) (1/20) (19/20) (19/20))
 
     , NS "ghci" "xterm -title ghci -e stack exec ghci" (title =? "ghci")
           (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
@@ -127,5 +128,8 @@ scratchpads = [
           (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
 
     , NS "gnus" "emacs-gtk -f gnus --title mail" (title =? "mail")
+          (customFloating $ W.RationalRect (1/20) (1/20) (17/20) (17/20))
+
+    , NS "spotify" "spotify" (className =? "Spotify")
           (customFloating $ W.RationalRect (1/20) (1/20) (17/20) (17/20))
     ]
